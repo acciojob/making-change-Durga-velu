f7d7b6031db96352a9082a1c4ff94c7c7cd46e7d
Durga-velu <durgadevi134babu@gmail.com>
2025-03-23 16:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const makeChange = (c) => {
   // your name here
-
+let ans={}
 let q=Math.floor(c/25);
 	c=c%25;
+	ans.push(`"q":${q}`)
 let d=Math.floor(c/10);
 	c=c%10;
+	ans.push(`"d":${d}`)
 let n=Math.floor(c/5);
 	c=c%5;
+	ans.push(`"n":${n}`)
 	let p=Math.floor(c/1);
 	c=c%1;
-	return `{q:${q}, d:${d}, n:${n}, p:${p}}`
+	ans.push(`"p":${p}`)
+	return ans
 };
 
 // Do not the change the code below
