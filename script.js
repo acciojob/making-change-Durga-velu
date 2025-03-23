976ef8ba4f13dc668c5ad11fd3c6989e96ef554c
Durga-velu <durgadevi134babu@gmail.com>
2025-03-23 16:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,5 +18,5 @@ let n=Math.floor(c/5);
 
 // Do not the change the code below
 
-const c = pompt("Enter c: ");
-rt(JSON.stringify(makeChange(c)));
+const c = prompt("Enter c: ");
+alert(JSON.stringify(makeChange(c)));
